0e9622efbe39a6632279502245daf2590a0e27eb
bLinD.cL <fe.vergaraa@gmail.com>
2019-06-01 14:17:41 -0400

    Version inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Felipe';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      case: '19'
+    }
+  };
+
+// tslint:disable-next-line: no-shadowed-variable
+  valorDePromesa = new Promise( ( resolve, reject ) => {
+    setTimeout( () => resolve('Llegó la data!'), 1000 );
+  });
+
+  fecha = new Date();
+
+  nombre2 = 'felipE eliAs vergarA albornOz';
+
+  video = 'mOeSfOJrUIk';
+
+  activar = true;
+
+  password = 'Felipe';
+
+  mostrar = true;
+
 }
